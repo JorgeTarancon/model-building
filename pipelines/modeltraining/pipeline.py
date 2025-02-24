55d9021bbd816668f0a2db53ff1131da849d88b0
Jorge <jtarancon@minsait.com>
2025-02-24 17:28:23 +0100

    test

--- pipelines/modeltraining/pipeline.py
+++ pipelines/modeltraining/pipeline.py
@@ -249,6 +249,33 @@ def get_pipeline(
         enable_caching=True,
         expire_after="P30d" # 30-day
     )
+
+    # for data quality check step
+    skip_check_data_quality = ParameterBoolean(name="SkipDataQualityCheck", default_value=False)
+    register_new_baseline_data_quality = ParameterBoolean(name="RegisterNewDataQualityBaseline", default_value=False)
+    supplied_baseline_statistics_data_quality = ParameterString(name="DataQualitySuppliedStatistics", default_value='')
+    supplied_baseline_constraints_data_quality = ParameterString(name="DataQualitySuppliedConstraints", default_value='')
+
+    # for data bias check step
+    skip_check_data_bias = ParameterBoolean(name="SkipDataBiasCheck", default_value = False)
+    register_new_baseline_data_bias = ParameterBoolean(name="RegisterNewDataBiasBaseline", default_value=False)
+    supplied_baseline_constraints_data_bias = ParameterString(name="DataBiasSuppliedBaselineConstraints", default_value='')
+
+    # for model quality check step
+    skip_check_model_quality = ParameterBoolean(name="SkipModelQualityCheck", default_value = False)
+    register_new_baseline_model_quality = ParameterBoolean(name="RegisterNewModelQualityBaseline", default_value=False)
+    supplied_baseline_statistics_model_quality = ParameterString(name="ModelQualitySuppliedStatistics", default_value='')
+    supplied_baseline_constraints_model_quality = ParameterString(name="ModelQualitySuppliedConstraints", default_value='')
+
+    # for model bias check step
+    skip_check_model_bias = ParameterBoolean(name="SkipModelBiasCheck", default_value=False)
+    register_new_baseline_model_bias = ParameterBoolean(name="RegisterNewModelBiasBaseline", default_value=False)
+    supplied_baseline_constraints_model_bias = ParameterString(name="ModelBiasSuppliedBaselineConstraints", default_value='')
+
+    # for model explainability check step
+    skip_check_model_explainability = ParameterBoolean(name="SkipModelExplainabilityCheck", default_value=False)
+    register_new_baseline_model_explainability = ParameterBoolean(name="RegisterNewModelExplainabilityBaseline", default_value=False)
+    supplied_baseline_constraints_model_explainability = ParameterString(name="ModelExplainabilitySuppliedBaselineConstraints", default_value='')
     ### PREPARATION ###
 
     ### PREPROCESS ###
@@ -278,6 +305,23 @@ def get_pipeline(
             test_score_threshold_param,
             model_package_group_name_param,
             tracking_server_arn_param,
+            skip_check_data_quality,
+            register_new_baseline_data_quality,
+            supplied_baseline_statistics_data_quality,
+            supplied_baseline_constraints_data_quality,
+            skip_check_data_bias,
+            register_new_baseline_data_bias,
+            supplied_baseline_constraints_data_bias,
+            skip_check_model_quality,
+            register_new_baseline_model_quality,
+            supplied_baseline_statistics_model_quality,
+            supplied_baseline_constraints_model_quality,
+            skip_check_model_bias,
+            register_new_baseline_model_bias,
+            supplied_baseline_constraints_model_bias,
+            skip_check_model_explainability,
+            register_new_baseline_model_explainability,
+            supplied_baseline_constraints_model_explainability
         ],
         steps=[step_preprocess],
         pipeline_definition_config=PipelineDefinitionConfig(use_custom_job_prefix=True)
