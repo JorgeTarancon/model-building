6e03b68d8f948ed2a5de2d8d9a0cb3a3c8f091d3
Jorge <jtarancon@minsait.com>
2025-02-25 11:46:53 +0100

    modify threshold for bias check steps

--- pipelines/modeltraining/pipeline.py
+++ pipelines/modeltraining/pipeline.py
@@ -359,7 +359,7 @@ def get_pipeline(
 
     # We are using this bias config to configure clarify to detect bias based on the first feature in the featurized vector
     data_bias_config = BiasConfig(
-        label_values_or_threshold=[1], facet_name=[1], facet_values_or_threshold=None
+        label_values_or_threshold=[1], facet_name=[1], facet_values_or_threshold=[[0.5]]
     )
 
     data_bias_check_config = DataBiasCheckConfig(
@@ -512,7 +512,7 @@ def get_pipeline(
 
     # We are using this bias config to configure clarify to detect bias based on the first feature in the featurized vector for Sex
     model_bias_config = BiasConfig(
-        label_values_or_threshold=[1], facet_name=[1], facet_values_or_threshold=None
+        label_values_or_threshold=[1], facet_name=[1], facet_values_or_threshold=[[0.5]]
     )
 
     model_bias_check_config = ModelBiasCheckConfig(
